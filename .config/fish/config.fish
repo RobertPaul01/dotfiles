aa7e92e07cd7246ac7301feb51d0a126d6fb04e1
Robert <robert20paul@gmail.com>
2018-01-06 22:41:00 -0600

    Fish update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,54 +2,50 @@ set source ~/.config/fish/config.fish
 
 # Functions
 function edit_source
-	vi $source 
+    vi $source 
 end
 
 function gitconfig
-	vi ~/.gitconfig
+    vi ~/.gitconfig
 end
 
 function reload_source
-	source $source 
-end
-
-function fish_prompt
-        printf '%s%s%s> ' (set_color purple) (prompt_pwd) (set_color normal)
+    source $source 
 end
 
 function get_perm
-	stat -f "%OLp" $args[1]
+    stat -f "%OLp" $args[1]
 end
 
 function grep_stderr --description 'Search stderr'
-	if count $argv | grep 2
-		eval $argv[1] 2>&1 | grep $argv[2]
-	else
-		echo 'Error: provide args for [executable, query]'
-	end
+    if count $argv | grep 2
+        eval $argv[1] 2>&1 | grep $argv[2]
+    else
+        echo 'Error: provide args for [executable, query]'
+    end
 end
 
 function kill_port
-	lsof -ti :$argv | xargs kill -9
+    lsof -ti :$argv | xargs kill -9
 end
 
 function clean_yarn_cache
-	npm cache clean --force
-	yarn cache clean
-	rm -rfd $TMPDIR/*
+    npm cache clean --force
+    yarn cache clean
+    rm -rfd $TMPDIR/*
 end
 
 function clean_dd
-	rm -rfd ~/Library/Developer/Xcode/DerivedData/*
+    rm -rfd ~/Library/Developer/Xcode/DerivedData/*
 end
 
 # Alias functions
 function lls
-	ls -lhaF
+    ls -lhaF
 end
 
 function vi
-	/usr/bin/vim $argv
+    /usr/bin/vim $argv
 end
 
 # OPAM configuration
